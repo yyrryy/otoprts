d72cacf5b805adbf55e6d4cd541fea7989da4171
Your Name <you@example.com>
2023-02-25 17:22:58 +0100

    'errffd'

--- autoparts/settings.py
+++ autoparts/settings.py
@@ -32,13 +32,29 @@ DEBUG = os.environ.get('DEBUG', False)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-ALLOWED_HOSTS = ['*']
-DATABASES = {
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    ALLOWED_HOSTS = ['134.122.58.13']
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'gadwad',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# production environment
+# DEBUG = False
+
 # # Database
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
